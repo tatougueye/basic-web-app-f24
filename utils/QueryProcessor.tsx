5c7895254062e57da4a25b5a44473a60a6e11de4
Fatou GUEYE <fatougueye@dyn-172-20-100-19.qatar.cmu.local>
2024-10-24 16:45:19 +0300

    added what is your name again

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,7 +13,7 @@ export default function QueryProcessor(query: string): string {
     return ( "fgueye" );
   }
 
-  if (query.toLowerCase().includes("What is your name?")) {
+  if (query.includes("What is your name?")) {
     return "fgueye-313";
   }
   return "";
